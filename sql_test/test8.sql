bedf4ecd8f3fb443612c09a776ec62caf924e088
KYJ1108 <108640336+KYJ1108@users.noreply.github.com>
2024-03-28 17:39:37 +0900

    Create test8.sql

---
+++ sql_test/test8.sql
@@ -0,0 +1,7 @@
+# 최근 반년 사이의 게시물 제목, 내용, 작성자 닉네임, 작성일 출력 . 작성일로 내림차순
+SELECT a.title AS '제목', a.`body` AS '제목', m.nname AS '닉네임', a.regDate AS '작성일'
+FROM article a
+  JOIN `member` m
+    ON a.mno = m.mno
+ WHERE a.regDate >= DATE_SUB(NOW(), INTERVAL 6 MONTH)
+ ORDER BY a.regDate DESC;
